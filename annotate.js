d4aedc68bab5846c2a671ba0ab7bb2d15b0dfd69
Andrew Lin <f50ajl@gmail.com>
2014-04-30 11:03:26 -0400

    comment out json in favor of iframe

--- annotate.js
+++ annotate.js
@@ -8,10 +8,20 @@ $(document).ready(function () {
       chrome.tabs.getSelected(null,function(tab) {
         var fullURL = tab.url.toString();
         $("#full-url").append( "<p>" + fullURL + "</p>" );
-        $.get('http://localhost:3000/comments?web_page=' + fullURL, 
-          function(returnedData){
-            $("#json-response").append(returnedData.shortened_url);
-        });
+
+        $('<iframe />', {
+            name: 'frame1',
+            id: 'frame1',
+            width: '100%',
+            height: '100%',
+            frameborder: '0',
+            src: 'http://localhost:3000/comments?web_page=' + fullURL
+        }).appendTo('body');
+
+        // $.get('http://localhost:3000/comments?web_page=' + fullURL, 
+        //   function(returnedData){
+        //     $("#json-response").append(returnedData.shortened_url);
+        // });
       });
     }
   }
